11e4cdbc8565a77526e4eabd9be826f9d4ebe540
JamesKamz <jacquesamouzou9@gmail.com>
2023-08-20 20:26:57 -0700

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main: Entry point
+ * code: content of code
+ * Return : Always 0 (success)
+ */
+int main(void)
+{
+char ch
+for (ch='a'; ch<='z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
+

f25331de8847d098529b2dc3cfca67dfe80b9be7
James kamz <81109024+JamesKamz@users.noreply.github.com>
2023-08-28 10:41:38 +0000

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*code: content of code
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'e' && ch != 'q')
+{
+putchar (ch);
+}
+}
+putchar ('\n');
+return (0);
+}

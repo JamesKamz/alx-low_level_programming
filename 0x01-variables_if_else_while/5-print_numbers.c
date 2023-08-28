be775fa0848683f995c4480f3726b8e127b9c694
James kamz <81109024+JamesKamz@users.noreply.github.com>
2023-08-28 10:47:29 +0000

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*code: content of code
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+{
+printf(num);
+}
+printf('\n');
+return (0);
+}

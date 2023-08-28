124a21fa95661d7bf1c124e68f3a8d03577b5819
James kamz <81109024+JamesKamz@users.noreply.github.com>
2023-08-28 10:29:25 +0000

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+*main -Entry point
+*code: content of code
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+printf('\n');
+return (0):
+}

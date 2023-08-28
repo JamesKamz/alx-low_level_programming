c9a2797d0af2c250c3a6791c41c57063fb1a49c5
JamesKamz <jacquesamouzou9@gmail.com>
2023-08-28 03:03:28 -0700

    correction of  preint alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main: Entry point
- * code: content of code
- * Return : Always 0 (success)
+ *main: Entry point
+ *code: content of code
+ *Return : Always 0 (success)
  */
 int main(void)
 {
 char ch
-for (ch='a'; ch<='z'; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
 }

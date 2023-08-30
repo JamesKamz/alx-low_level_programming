feb71113e86ff39eafe9864ec70f267e402813be
JamesKamz <jacquesamouzou9@gmail.com>
2023-08-30 10:15:40 -0700

    correction of

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 int digit0, digit1;
-for (digit0 = 0; digit1 < 9; digit0++)
+for (digit0 = 0; digit0 < 9; digit0++)
 {
-for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+for (digit1 = digit0 + 1; digit1 < 10; digit1++)
 {
+putchar((digit0 % 10) + '0');
 putchar((digit1 % 10) + '0');
-putchar((digit2 % 10) + '0');
-if (digit1 == 8 && digit2 == 9)
+if (digit0 == 8 && digit1 == 9)
 {
 continue;
 }

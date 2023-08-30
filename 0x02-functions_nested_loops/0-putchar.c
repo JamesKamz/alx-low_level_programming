8b593e5d26ea9f3a02ab26a275f55cc85c85165d
JamesKamz <jacquesamouzou9@gmail.com>
2023-08-30 11:05:35 -0700

    print

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
 #include "main.h"
 /**
- *main - start point
+ *main - main point
  *code: content of code
  *Return: always (0)
  */
-int main(void){
-printf('_putchar \n');
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
 

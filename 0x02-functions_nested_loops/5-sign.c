1ce5df9287fafa4e2d053145c5622962c21cad6d
JamesKamz <jacquesamouzou9@gmail.com>
2023-09-05 05:23:09 -0700

    check a sign of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ * @n: parameter
+ * Return: 1 if is greater than zero
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n = 0)
+{
+_putchar(48);
+return (0);
+}
+else
+{
+_putchar(45);
+return (-1);
+}
+_putchar('\n');
+}
+

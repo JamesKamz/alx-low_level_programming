446e51fbcc35c10b54bacbe737b75041f38eb03f
JamesKamz <jacquesamouzou9@gmail.com>
2023-09-05 03:45:39 -0700

    print ten times alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * code: content of code
+ * Return: always (0)
+ */
+void print_alphabet_x10(void)
+{
+char ch
+int i
+for ( i = 0; i < 10; i++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
+return (0);
+}
+

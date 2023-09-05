8f2900ac521c98cb13eb4a2095e59c509166c092
JamesKamz <jacquesamouzou9@gmail.com>
2023-09-05 10:30:52 -0700

    removing

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype>
+/**
+ * _isupper - Entry main
+ *@c: parameter
+ *
+ * Return: 0 or 1
+ */
+int _isupper (int c)
+{
+if (isupper(c))
+{
+return (1);
+}
+else 
+return (0)
+}
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Readme for more functions nested and loops project

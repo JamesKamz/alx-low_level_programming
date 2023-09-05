da64632aa352e539c2c89d9ffe8177f7edd6b19a
JamesKamz <jacquesamouzou9@gmail.com>
2023-09-05 04:57:20 -0700

    check if is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - returns 1 if c is lowercase and 0 otherwise
+ */
+int _islower(int c)
+{
+if (islower(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
+
